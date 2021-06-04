f8a4ed98d53916b5aa230c612a2edada70cb58f5
LILIYA SUJO <liliyasujo@gmail.com>
2021-06-04 20:00:42 +0530

    Day 3

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,17 +6,34 @@ class HomePageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TopBar(
-              title: 'Liliya Sujo',
-              subtitle: 'Developer',
-              color: Color(0xff0B3D2E),
-            ),
-            const SizedBox(height: 10),
-            Text('Photography',style: TextStyle(fontSize: 22,color: Color(0xff0B3D2E)),),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TopBar(
+                title: 'Liliya Sujo',
+                subtitle: 'Developer',
+                color: Color(0xff0B3D2E),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Photography',
+                style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
+              ),
+              GridView.builder(
+                itemCount: 4,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (ctx, index) => Container(
+                  padding: EdgeInsets.all(10),
+                  child: Image.network(
+                      'https://images.unsplash.com/photo-1559563362-c667ba5f5480?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=945&q=80'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
